23a152c343f5e1bb79f2aaee118e748c523ae685
ria-27 <riavasishta@gmail.com>
2026-01-10 17:05:57 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,7 @@
+colour=input("enter a colour: ")
+plural_noun=input("enter a plural noun: ")
+celebrity=input("enter a celebrity: ")
+
+print("Roses are " + colour)
+print(plural_noun + " are blue")
+print("I love "+celebrity)
