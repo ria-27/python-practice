f9d20a0d4e60e04302c162bca7e9228a9d125442
ria-27 <riavasishta@gmail.com>
2026-01-26 19:50:35 +0530

    Add files via upload

---
+++ largest of 3.py
@@ -0,0 +1,9 @@
+# Program to find largest of 3 numbers
+def largest(num1,num2,num3):
+    if num1>=num2 and num1>=num3:
+        return num1
+    elif num2>=num1 and num2>=num3:
+        return num2
+    else:
+        return num3
+print(largest(3,4,5))
